431fe106ad93ebb3dcf945656e84c3917e6411b7
Long32 <99838967+Long132000@users.noreply.github.com>
2025-02-13 13:59:47 +0300

    Delete lesson_3_task_1.py

--- lesson_3_task_1.py
+++
@@ -1,9 +0,0 @@
-from user import User
-
-# Создаем экземпляр класса User
-my_user = User("Иван", "Иванов")
-
-# Вызываем методы объекта
-my_user.print_first_name()  # Вывод имени
-my_user.print_last_name()   # Вывод фамилии
-my_user.print_full_name()   # Вывод имени и фамилии
