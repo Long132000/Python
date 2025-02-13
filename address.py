1916abf456cbb660fc381b9f96e0e45920820c10
Long32 <99838967+Long132000@users.noreply.github.com>
2025-02-13 13:59:38 +0300

    Delete address.py

--- address.py
+++
@@ -1,8 +0,0 @@
-class Address:
-    # Конструктор класса, принимающий индекс, город, улицу, дом и квартиру
-    def __init__(self, index, city, street, house, apartment):
-        self.index = index    # Индекс
-        self.city = city      # Город
-        self.street = street  # Улица
-        self.house = house    # Дом
-        self.apartment = apartment  # Квартира
